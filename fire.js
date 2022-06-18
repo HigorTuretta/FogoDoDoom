9b214a65434ea69a9515b82b56a534fd833d4bd4
Higor Turetta <69050147+HigorTuretta@users.noreply.github.com>
2022-06-18 13:47:28 -0300

    Update fire.js

--- fire.js
+++ fire.js
@@ -4,15 +4,15 @@ const fireHeight = 45
 const fireColorsPallete =  ['rgb(7, 7, 7)', 'rgb(31, 7, 7)', 'rgb(47, 15, 7)', 'rgb(71, 15, 7)', 'rgb(87, 23, 7)', 'rgb(103, 31, 7)', 'rgb(119, 31, 7)', 'rgb(143, 39, 7)', 'rgb(159, 47, 7)', 'rgb(175, 63, 7)', 'rgb(191, 71, 7)', 'rgb(199, 71, 7)', 'rgb(223, 79, 7)', 'rgb(223, 87, 7)', 'rgb(223, 87, 7)', 'rgb(215, 95, 7)', 'rgb(215, 95, 7)', 'rgb(215, 103, 15)', 'rgb(207, 111, 15)', 'rgb(207, 119, 15)', 'rgb(207, 127, 15)', 'rgb(207, 135, 23)', 'rgb(199, 135, 23)', 'rgb(199, 143, 23)', 'rgb(199, 151, 31)', 'rgb(191, 159, 31)', 'rgb(191, 159, 31)', 'rgb(191, 167, 39)', 'rgb(191, 167, 39)', 'rgb(191, 175, 47)', 'rgb(183, 175, 47)', 'rgb(183, 183, 47)', 'rgb(183, 183, 55)', 'rgb(207, 207, 111)', 'rgb(223, 223, 159)', 'rgb(239, 239, 199)', 'rgb(255, 255, 255)']
 const controlRange = document.getElementById('range')
 const sound = document.getElementById('som');
-let rangeValue = 0.5
-sound.loop = true
+let rangeValue = 4
 
 controlRange.addEventListener('input', (evt) =>{
     rangeValue = evt.target.value
-    if(rangeValue > 0.5 ){
-        sound.volume -= 0.1;
+
+    if(rangeValue > 5 ){
+        sound.volume = 0.3;
     }else{
-        sound.volume += 0.1;
+        sound.volume = 0.7;
     }
 })
 
@@ -49,7 +49,7 @@ function updateFireIntensityPerPixel(currentPixelIndex){
     if(belowPixelIndex > fireWidth * fireHeight){
         return
     }
-    const decay = Math.floor(Math.random() * (rangeValue * 400) )
+    const decay = Math.floor(Math.random() * rangeValue )
     const belowPixelFireIntensity = firePixelArray[belowPixelIndex] 
     const newFireIntenisty = belowPixelFireIntensity - decay  >= 0 ? belowPixelFireIntensity - decay : 0
 
@@ -93,7 +93,4 @@ function createFireSource() {
     }
 }
 
-window.onload = function() {
-    start();
-};
-
+start();
